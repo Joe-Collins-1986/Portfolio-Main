ba98db9236bfb97718c91b90c209d9731f63eada
Joseph Collins <joecollins@Josephs-MBP.home>
2023-08-06 18:34:15 +0100

    add contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "../styles/Contact.module.css";
+import uStyles from "../styles/MultiUse.module.css";
+
+const Contact = () => {
+  return (
+    <section className={styles.contact} id="contact">
+      <h2 className={uStyles.heading}>
+        Contact <span>Me</span>
+      </h2>
+
+      <form action="#">
+        <div className={styles.inputBox}>
+          <div className={styles.inputField}>
+            <input type="text" placeholder="Full Name*" required />
+            <span className={styles.focus}></span>
+          </div>
+
+          <div className={styles.inputField}>
+            <input type="text" placeholder="Email Address*" required />
+            <span className={styles.focus}></span>
+          </div>
+        </div>
+
+        <div className={styles.inputBox}>
+          <div className={styles.inputField}>
+            <input type="number" placeholder="Mobile Number" />
+            <span className={styles.focus}></span>
+          </div>
+
+          <div className={styles.inputField}>
+            <input type="text" placeholder="Email Subject*" required />
+            <span className={styles.focus}></span>
+          </div>
+        </div>
+
+        <div className={styles.textareaField}>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Your Message*"
+            required
+          ></textarea>
+          <span className={styles.focus}></span>
+        </div>
+
+        <div className={`${uStyles.btnBox} ${uStyles.btns}`}>
+          <button type="submit" className={uStyles.btn}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
